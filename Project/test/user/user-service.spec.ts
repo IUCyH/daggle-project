f4067a28bdd719cf06d54ff7c4eadcb228edd106
Jaemin <ghdckdwn8456@gmail.com>
2025-04-23 19:12:41 +0900

    [feat] 실제 db 테스트로 변경, 코드 일부 수정

--- Project/test/user/user-service.spec.ts
+++ Project/test/user/user-service.spec.ts
@@ -1,5 +1,5 @@
 import { Test } from "@nestjs/testing";
-import { getRepositoryToken } from "@nestjs/typeorm";
+import { getRepositoryToken, TypeOrmModule } from "@nestjs/typeorm";
 import mockUserRepository from "./mock-user.repository";
 
 import { IUserService } from "../../src/features/user/interface/user-service.interface";
@@ -11,23 +11,50 @@ import { User } from "../../src/features/user/entity/user.entity";
 
 import { NotFoundException } from "../../src/common/exceptions/not-found.exception";
 import { BadRequestException } from "../../src/common/exceptions/bad-request.exception";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
+import { Repository } from "typeorm";
 
 describe("UserService", () => {
     let userService!: IUserService;
+    let userRepository: Repository<User>;
 
     beforeAll(async () => {
         const module = await Test.createTestingModule({
+            imports: [
+                TypeOrmModule.forRootAsync({
+                    useFactory: () => ({
+                        type: "postgres",
+                        host: "localhost",
+                        port: 5432,
+                        username: "test",
+                        password: "abc123",
+                        database: "test",
+                        synchronize: false,
+                        logging: true,
+                        namingStrategy: new SnakeNamingStrategy(),
+                        extra: {
+                            timezone: "Z",
+                            dateStrings: true
+                        },
+                        entities: [__dirname + "/../../src/**/*.entity.{js,ts}"],
+                        subscribers: [],
+                        migrations: []
+                    })
+                }),
+                TypeOrmModule.forFeature([User])
+            ],
             providers: [
                 UserService,
-                HashHelperService,
-                {
-                    provide: getRepositoryToken(User),
-                    useValue: mockUserRepository
-                }
+                HashHelperService
             ]
         }).compile();
 
         userService = module.get<UserService>(UserService);
+        userRepository = module.get<Repository<User>>(getRepositoryToken(User));
+    });
+
+    afterEach(async () => {
+        await userRepository.clear();
     });
 
     it("유저 생성 후 같은 id로 찾은 유저의 이메일은 동일해야 함", async () => {
@@ -42,7 +69,7 @@ describe("UserService", () => {
         expect(foundUser.email).toEqual(user.email);
     });
 
-    it("이미 생성된 유저이고, deletedAt이 null 이라면 에러가 반환되야 함", async () => {
+    it("이미 생성된 유저이고, deletedAt이 null 이라면 BadRequestException 에러가 반환되야 함", async () => {
         // given
         const user = createTestUser();
 
@@ -53,7 +80,6 @@ describe("UserService", () => {
         await expect(userService.createUser(user)).rejects.toThrow(BadRequestException);
     });
 
-    // TODO: 실제 DB 연결 후 아래 테스트 코드 실행
     it("유저 업데이트 후 변경사항이 잘 반영되야 함", async () => {
         // given
         const user = createTestUser();
@@ -66,7 +92,7 @@ describe("UserService", () => {
         updateUserDto.password = user.password;
 
         // when
-        updateUserDto.nickname = "updated_nickname";
+        updateUserDto.nickname = "updatenickname";
         await userService.updateUser(userId, updateUserDto);
 
         // then
@@ -84,7 +110,7 @@ describe("UserService", () => {
         await userService.deleteUser(userId);
 
         // then
-        expect(await userService.getUserById(userId)).toThrow(); // TODO: 서비스 에러 정의 후 채워넣기
+        await expect(userService.getUserById(userId)).rejects.toThrow(NotFoundException);
     });
 });
 
