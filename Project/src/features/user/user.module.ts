0c89a280ffa58183fa30888d9ca1be154c6b75e8
Jaemin <ghdckdwn8456@gmail.com>
2025-04-24 21:42:23 +0900

    [fix] user module 의존성 문제 수정

--- Project/src/features/user/user.module.ts
+++ Project/src/features/user/user.module.ts
@@ -5,10 +5,11 @@ import { USER_SERVICE } from "./interface/user-service.interface";
 import { UserService } from "./user.service";
 import { HashHelperService } from "../../common/helpers/hash-helper.service";
 import { User } from "./entity/user.entity";
+import { TokenInfo } from "../../common/auth/tokenVerification/entity/token-info.entity";
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([User])
+        TypeOrmModule.forFeature([User, TokenInfo])
     ],
     controllers: [UserController],
     providers: [
