9c9071a9a735104de7a76f6689af3add1345f250
Jaemin <ghdckdwn8456@gmail.com>
2025-04-27 04:19:16 +0900

    Update app.module.ts

--- Project/src/app.module.ts
+++ Project/src/app.module.ts
@@ -39,7 +39,7 @@ import path from "path";
             useFactory: () => ({
                 type: "postgres",
                 host: process.env.DB_HOST,
-                port: process.env.DB_PORT,
+                port: 5432,
                 username: process.env.DB_USERNAME,
                 password: process.env.DB_PASSWORD,
                 database: process.env.DB_NAME,
